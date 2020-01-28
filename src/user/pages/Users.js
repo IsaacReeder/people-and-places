d61f5769a6d45bf9781e14031f1277be200efe6a
Isaac R <isaacreeder5@gmail.com>
2020-01-28 10:43:28 -0800

    added a users list page / components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <div>Users works</div>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Bob Gustafson",
+      image:
+        "https://lh4.googleusercontent.com/-anbU0gkIlZo/AAAAAAAAAAI/AAAAAAAAABo/2oLoyiVEUGc/photo.jpg",
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;

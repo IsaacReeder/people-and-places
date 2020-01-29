be6f4e97947417808eb19adc8cea00c27454ce07
Isaac R <isaacreeder5@gmail.com>
2020-01-29 13:04:24 -0800

    added opening and closing animation on the drawer in mobile view

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./SideDrawer.css";
 
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+
 const SideDrawer = props => {
-  const content = <aside className="side-drawer">{props.children}</aside>;
+  const content = (
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={props.onClick}>
+        {props.children}
+      </aside>
+    </CSSTransition>
+  );
 
   //   below is a portal that you can find in index.html right above the root tag. the first argument below is whats being rendered, second is where
   return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));

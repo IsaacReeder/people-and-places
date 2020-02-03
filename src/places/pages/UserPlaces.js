f2848bf01cdb2adc385925e10c4ce2d189b55b5d
Isaac R <isaacreeder5@gmail.com>
2020-02-03 11:44:36 -0800

    added custom styled buttons

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
 
@@ -31,8 +32,11 @@ const DUMMY_PLACES = [
   }
 ];
 
+// code below filters javascript objects into place, and pulls the user ID to identify which places were posted by a single user
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
